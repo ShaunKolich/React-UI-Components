476c8e51d72b6a4aa33e43f30c2c2d82041a44d6
ShaunKolich <employmeinc@gmail.com>
2019-05-29 20:55:16 -0400

    Creating React App

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+import ImgThumbnail from './ImageThumbnail';
+
+function HeaderContainer() {
+    return (<div className='MainContainer' >
+        <section class="HeaderContainer" >
+            <section class='LogoImg'><ImgThumbnail/></section>
+            <section class='ContentMain'><HeaderContent/></section>
+            </section>
+    </div>
+
+    )
+
+}
+export default HeaderContainer;
