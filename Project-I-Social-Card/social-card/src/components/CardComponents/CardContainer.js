8b85bf5a1a81d273f75d6d3c956d91403ce363a1
ShaunKolich <employmeinc@gmail.com>
2019-05-29 21:53:17 -0400

    Completed Project

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,4 +1,19 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 
-function Card
+function CardContainer() {
+    return (
+        <div className='MainCardContainer'>
+            <section className='CardContainer'><CardBanner />
+
+                <section><CardContent />
+                </section>
+            </section>
+        </div>
+
+            )
+
+        }
+export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent() {
+    return (
+        <div className='CardContent'>
+            <h2>Get Start with React</h2>
+            <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+            <a href="https://reactjs.org/">reactjs.org</a>
+        </div>
+    )
+}
+export default CardContent;
